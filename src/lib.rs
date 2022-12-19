0ad16d8edb212b7527edca91e9e9b995b29e5a2f
Warlock <internalmike@gmail.com>
2022-12-19 16:18:19 +0300

    code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,79 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![deny(warnings)]
+
+#![cfg_attr(not(test), no_std)]
+
+use serde::{Serialize, Serializer};
+
+pub trait SerializeSeed {
+    type Value;
+
+    fn serialize<S: Serializer>(&self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error>;
+}
+
+pub struct ValueWithSeed<'a, Value, Seed>(pub &'a Value, pub Seed);
+
+impl<'a, Value, Seed: SerializeSeed<Value=Value>> Serialize for ValueWithSeed<'a, Value, Seed> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.1.serialize(self.0, serializer)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde::de::DeserializeSeed;
+
+    mod complex_type {
+        use crate::SerializeSeed;
+        use serde::{Deserializer, Serializer};
+        use serde::de::{self, DeserializeSeed};
+        use std::fmt::{self, Formatter};
+
+        pub struct ComplexType(pub u8);
+
+        pub struct ComplexTypeSerde {
+            pub xor: u8,
+        }
+
+        struct ComplexTypeDeVisitor(ComplexTypeSerde);
+
+        impl<'de> de::Visitor<'de> for ComplexTypeDeVisitor {
+            type Value = ComplexType;
+
+            fn expecting(&self, f: &mut Formatter) -> fmt::Result {
+                write!(f, "u8")
+            }
+
+            fn visit_u64<E: de::Error>(self, v: u64) -> Result<Self::Value, E> {
+                let v = u8::try_from(v).map_err(|_| E::invalid_value(de::Unexpected::Unsigned(v), &self))?;
+                Ok(ComplexType(v ^ self.0.xor))
+            }
+        }
+
+        impl<'de> DeserializeSeed<'de> for ComplexTypeSerde {
+            type Value = ComplexType;
+
+            fn deserialize<D: Deserializer<'de>>(self, deserializer: D) -> Result<Self::Value, D::Error> {
+                deserializer.deserialize_u8(ComplexTypeDeVisitor(self))
+            }
+        }
+
+        impl SerializeSeed for ComplexTypeSerde {
+            type Value = ComplexType;
+
+            fn serialize<S: Serializer>(&self, value: &Self::Value, serializer: S) -> Result<S::Ok, S::Error> {
+                serializer.serialize_u8(value.0 ^ self.xor)
+            }
+        }
+    }
+
+    use complex_type::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let x = ComplexType(10);
+        let json = serde_json::to_value(ValueWithSeed(&x, ComplexTypeSerde { xor: 0x34 })).unwrap();
+        let y = ComplexTypeSerde { xor: 0x34 }.deserialize(json).unwrap();
+        assert_eq!(x.0, y.0);
     }
 }
